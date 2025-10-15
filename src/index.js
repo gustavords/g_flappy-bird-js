52b772d2802c8d1e9c3e812f09b3b6b3cd531806
gustavords <22622146+gustavords@users.noreply.github.com>
2025-10-14 21:18:11 -0400

    Add collision and multiple objects

--- src/index.js
+++ src/index.js
@@ -1,155 +1,219 @@
 import "./styles.css";
 
-console.log( `in here` );
-
-
-let myGamePiece;
-let myObstacle;
-// let myObstacles=[];
-let test;
-// let key;
-let keys;
-
-
-function startGame ()
+class Screen
 {
+  canvas_el = document.getElementById( `canvas` );
+  canvas_ctx = this.canvas_el.getContext( `2d` );
+  width = this.canvas_el.width;
+  height = this.canvas_el.height;
+  state = () => { return setInterval( gameState, 50 ) };
+  frame = 0;
 
-  window.addEventListener( 'keydown', function ( e )
+  clearCanvas ()
   {
-    keys = ( keys || [] );
-    keys[ e.key ] = ( e.type == "keydown" );
-  } )
-  window.addEventListener( 'keyup', function ( e )
+    this.canvas_ctx.clearRect( 0, 0, this.width, this.height );
+  }
+
+  stop ( state )
   {
-    // key = false;
-    keys[ e.key ] = ( e.type == "keydown" );
-  } )
-
-  myGamePiece = new component( 30, 30, `red`, 10, 120 );
-  myObstacle = new component( 40, 200, `green`, 300, 120 );
-  myGameArea().setInterval;
-
-  myGameArea().context;
-
+    clearInterval( state );
+  }
 }
 
-function component ( width, height, color, x, y )
+//should be other name since its all rectangles
+class GameObject extends Screen
 {
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-
-  this.update = () =>
+  constructor ( positionX, positionY, colour, width, height )
   {
-    const ctx = myGameArea().context;
-    ctx.fillStyle = color;
-    ctx.fillRect( this.x, this.y, this.width, this.height );
-
+    super();
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.colour = colour;
+    this.width = width;
+    this.height = height;
   }
-  this.newPos = () =>
+  drawObject ()
   {
-    this.x += this.speedX;
-    if ( this.x >= ( myGameArea().width - 30 ) )
-    {
-      this.x = ( myGameArea().width - 30 );
-    }
-    else if ( this.x <= 0 )
-    {
-      this.x = 0;
-    }
-    else
-    {
-      this.x += this.speedX;
-    }
-
-    this.y += this.speedY;
-    if ( this.y >= ( myGameArea().height - 30 ) )
-    {
-      this.y = ( myGameArea().height - 30 );
-    }
-    else if ( this.y <= 0 )
-    {
-      this.y = 0;
-    }
-    else
-    {
-      this.y += this.speedY;
-    }
+    this.canvas_ctx.fillStyle = this.colour;
+    this.canvas_ctx.fillRect( this.positionX, this.positionY, this.width, this.height )
   }
 
-  this.crashWith = function ( otherobj )
+  collisionDetected ( gameObject )
   {
-    var myleft = this.x;
-    var myright = this.x + ( this.width );
-    var mytop = this.y;
-    var mybottom = this.y + ( this.height );
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + ( otherobj.width );
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + ( otherobj.height );
-    var crash = true;
-    if ( ( mybottom < othertop ) ||
-      ( mytop > otherbottom ) ||
-      ( myright < otherleft ) ||
-      ( myleft > otherright ) )
+    let obj_a_left = this.positionX;
+    let obj_a_right = this.positionX + this.width;
+    let obj_a_top = this.positionY;
+    let obj_a_bottom = this.positionY + this.height;
+
+    let obj_b_left = gameObject.positionX;
+    let obj_b_right = gameObject.positionX + gameObject.width;
+    let obj_b_top = gameObject.positionY;
+    let obj_b_bottom = gameObject.positionY + gameObject.height;
+
+    let crash = true;
+
+    if ( ( obj_a_bottom < obj_b_top ) ||
+      ( obj_a_top > obj_b_bottom ) ||
+      ( obj_a_right < obj_b_left ) ||
+      ( obj_a_left > obj_b_right ) )
     {
       crash = false;
     }
+
     return crash;
   }
-}
 
-
-function myGameArea ()
-{
-  const canvas_el = document.getElementById( `canvas` );
-  const context = canvas_el.getContext( `2d` );
-  const interval = setInterval( updateGameArea, 20 );
-  const frameNo = 0;
-
-  return {
-    context,
-    width: canvas_el.width,
-    height: canvas_el.height,
-    interval,
-    frameNo,
-    clear: () => { context.clearRect( 0, 0, canvas_el.width, canvas_el.height ) },
-    stop: () => { clearInterval( interval ) },
-  }
-}
-
-function updateGameArea ()
-{
-  if ( myGamePiece.crashWith( myObstacle ) )
+  updatePosition = () =>
   {
-    myGameArea().stop();
-  }
-  else
-  {
-    myGameArea().clear();
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-    if ( keys && keys[ `ArrowLeft` ] ) { myGamePiece.speedX = -.1; }
-    if ( keys && keys[ `ArrowRight` ] ) { myGamePiece.speedX = .1; }
-    if ( keys && keys[ `ArrowUp` ] ) { myGamePiece.speedY = -.2; }
-    if ( keys && keys[ `ArrowDown` ] ) { myGamePiece.speedY = .2; }
-    myGamePiece.newPos();
-    myObstacle.x += -.2;
-    myObstacle.update();
-    myGamePiece.update();
-  }
+    this.positionX += this.speedX;
+    if ( this.positionX >= ( gameArea.width - this.width ) )
+    {
+      this.positionX = ( gameArea.width - this.width );
+      this.speedX = 0;
+    }
+    else if ( this.positionX <= 0 )
+    {
+      this.positionX = 0;
+      this.speedX = 0;
+    }
+    else
+    {
+      this.positionX += this.speedX;
+    }
 
+    this.positionY += this.speedY;
+    if ( this.positionY >= ( gameArea.height - this.height ) )
+    {
+      this.positionY = ( gameArea.height - this.height );
+      this.speedY = 0;
+    }
+    else if ( this.positionY <= 0 )
+    {
+      this.positionY = 0;
+      this.speedY = 0;
+    }
+    else
+    {
+      this.positionY += this.speedY;
+    }
+  }
 }
 
-function everyinterval ( n )
+//can go in scene
+function everyInterval ( n )
 {
-  if ( ( myGameArea().frameNo / n ) % 1 == 0 ) { return true; }
+  if ( ( gameArea.frame / n ) % 1 == 0 )
+  {
+    return true;
+  }
   return false;
+
+}
+
+//can go in gameObject
+function drawMultipleObjects ()
+{
+
+  gameArea.frame++;
+  if ( gameArea.frame == 1 || everyInterval( 200 ) )
+  {
+    console.log( `here` );
+    const random = ( min, max ) => { return Math.floor( ( Math.random() * ( max - min ) ) + min ) };
+    const randomObstacleHeight = random( 100, 425 );
+    const apposingHeight = ( ( randomObstacleHeight + 100 ) - gameArea.height ) * -1;
+    console.log( randomObstacleHeight, apposingHeight );
+    obstacleArr.push( new GameObject( gameArea.width, 0, `green`, 50, randomObstacleHeight ) );
+    obstacleArr.push( new GameObject( gameArea.width, gameArea.height - apposingHeight, `green`, 50, apposingHeight ) );
+  }
+  obstacleArr.forEach( ( obstacle ) =>
+  {
+    obstacle.positionX -= 1;
+    obstacle.drawObject();
+  } )
+
+}
+
+const gameArea = new Screen();
+const bird = new GameObject( 0, 0, `blue`, 50, 50 );
+const obstacleArr = [];
+
+let state = gameArea.state();
+
+function gameState ()
+{
+  gameArea.clearCanvas();
+  drawMultipleObjects();
+
+  obstacleArr.forEach( ( obstacle ) =>
+  {
+    if ( bird.collisionDetected( obstacle ) )
+    {
+      gameArea.stop( state );
+      return;
+    }
+    obstacle.stop();
+  } );
+  bird.stop();
+  gameControls( bird );
+  bird.updatePosition();
+  bird.drawObject();
+
 }
 
 
+function gameControls ( gameObject )
+{
 
-startGame();
+  window.addEventListener( `keydown`, ( e ) =>
+  {
+    speedChange( e, 2 );
+  } );
+
+  window.addEventListener( `keyup`, ( e ) =>
+  {
+    zeroSpeed();
+  } );
+
+  function zeroSpeed ()
+  {
+    gameObject.speedX = 0;
+    gameObject.speedY = 0;
+    window.removeEventListener( `keyup`, zeroSpeed );
+
+  }
+  function speedChange ( event, velocity )
+  {
+    if ( event.defaultPrevented )
+    {
+      return; // Do nothing if the event was already processed
+    }
+    if ( !event.repeat )
+    {
+      switch ( event.key )
+      {
+        case `ArrowUp`:
+          gameObject.speedY = -velocity;
+
+          break;
+        case `ArrowDown`:
+          gameObject.speedY = velocity;
+          break;
+        case `ArrowLeft`:
+          gameObject.speedX = -velocity;
+          break;
+        case `ArrowRight`:
+          gameObject.speedX = velocity;
+          break;
+      }
+
+      event.preventDefault();
+      return;
+    }
+
+    window.removeEventListener( `keydown`, speedChange );
+
+  }
+
+}
